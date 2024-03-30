5b1c2f19a57efe745b91d36f5d8ba7641c0a64e4
Rahul Raj <rraj45022@gmail.com>
2024-03-30 18:22:16 +0530

    React Project

---
+++ src/App.tsx
@@ -0,0 +1,36 @@
+import { useState } from "react"
+import  CategoryPills from "./components/Category"
+import { categories, videos } from "./data/home"
+import { PageHeader } from "./layout/PageHeader"
+import  VideoGridItem  from "./components/VideoGridItem"
+import  Sidebar  from "./layout/SideBar"
+import { SidebarProvider } from "./context/SidebarContext"
+
+export default function App() {
+  const [selectedCategory, setSelectedCategory] = useState(categories[0])
+
+  return (
+    <SidebarProvider>
+      <div className="max-h-screen flex flex-col">
+        <PageHeader />
+        <div className="grid grid-cols-[auto,1fr] flex-grow-1 overflow-auto">
+          <Sidebar />
+          <div className="overflow-x-hidden px-8 pb-4">
+            <div className="sticky top-0 bg-white z-10 pb-4">
+              <CategoryPills
+                categories={categories}
+                selectedCategory={selectedCategory}
+                onSelect={setSelectedCategory}
+              />
+            </div>
+            <div className="grid gap-4 grid-cols-[repeat(auto-fill,minmax(300px,1fr))]">
+              {videos.map(video => (
+                <VideoGridItem key={video.id} {...video} />
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </SidebarProvider>
+  )
+}
